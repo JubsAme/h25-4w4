30b16bffb67c529cca8dc0a2dedd778494afb141
JubsAme <e1965295@cmaisonneuve.qc.ca>
2025-03-12 10:36:44 -0400

    Ajout des customizer pour le footer et la section hero

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<?php $footer_couleurIcones = get_theme_mod('footer_couleurIcones', '');?>
 <footer>
     <div class="piedpage global">
         <section class="piedpage__s1">
@@ -10,12 +11,16 @@
             <div class="piedpage__s1__adresse">
                 <div class="piedpage__s1__adresse__coord">
                     Trouvez nos informations et rejoignez-nous sur nos réseaux sociaux!
-                    <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
-                    <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
-                    <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
-                    <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
+            <!---Ici mettre des variables controlables avec les Customizer-->
+                    <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=<?php echo $footer_couleurIcones ?>" width="20" height="20">
+                    <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=<?php echo $footer_couleurIcones ?>" width="20" height="20">
+                    <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=<?php echo $footer_couleurIcones ?>" width="20" height="20">
+                    <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=<?php echo $footer_couleurIcones ?>" width="20" height="20">
                 </div>
                 <div class="piedpage__s1__adresse__recherche">
+                    <h3 class="footerAdresse">
+                        3800 rue Sherbrooke Est.
+                    </h3>
                     <?php get_search_form();   ?>
                 </div>
             </div>
@@ -23,8 +28,12 @@
                 Chez Jubs Airways notre mission est de vous offrir des plans de voyages affordables accompagné par l'expertise de nos employés pour vous offrir des vacances de rêve!
             </div>
         </section>
-        <section class="piedpage__s2"></section>
-        <section class="piedpage__s3"></section>
+        <section class="piedpage__s2">
+
+        </section>
+        <section class="piedpage__s3">
+
+        </section>
 
 
     </div>
