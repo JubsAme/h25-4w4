014d51f24529a0d2c0a11bf76a3df3d1ec66199b
JubsAme <e1965295@cmaisonneuve.qc.ca>
2025-03-12 12:26:24 -0400

    Mettre l'image thumbnail en arriere plan dans les cartes

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -5,16 +5,22 @@
 ?>
 <article class="carte carte--grande">
   <figure class="carte__image">
+  <?php
+        if (has_post_thumbnail()) {
+        the_post_thumbnail('medium'); 
+      } 
+    ?> 
     </figure>
   <div class="carte__contenu">
-    <?php
-        if (has_post_thumbnail()) {
-        the_post_thumbnail('thumbnail'); }
-    ?>    
-    <h4 class="carte__titre"><?php the_title(); ?></h4>
+   <div class="carte__conteneurTitre">
+   <h4 class="carte__titre"><?php the_title(); ?></h4>
+   </div>
     <p class="carte__description"><?php echo wp_trim_words(get_the_content(),10, " ... " ); ?></p>
-    <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">suite ...</a>
     <?php the_category()?>
     <p>Température Maximum: <?php the_field("temperature_maximum"); ?>°C</p>
+    <p>Température Minimum: <?php the_field("temperature_minimum"); ?>°C</p>
+    <p>Température Moyenne: <?php the_field("temperature_moyenne"); ?>°C</p>
+    <p class="Prix">Prix: <?php the_field("prix_voyage"); ?>$</p>
+    <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">suite ...</a>
   </div>
 </article>
--- single-post.php
+++ single-post.php
@@ -13,6 +13,7 @@
                 <p>Température Maximum: <?php the_field("temperature_maximum"); ?>°C</p>
                 <p>Température Minimum: <?php the_field("temperature_minimum"); ?>°C</p>
                 <p>Température Moyenne: <?php the_field("temperature_moyenne"); ?>°C</p>
+                <p>Prix: <?php the_field("prix_voyage"); ?>$</p>
             <?php endwhile; endif; ?>
         </div>
     </section>
