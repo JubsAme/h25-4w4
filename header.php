3ba91872e8c251f05406da5d1f3e74a474efaf00
JubsAme <e1965295@cmaisonneuve.qc.ca>
2025-03-18 18:25:02 -0400

    Ajout d'un bouton burger SVG

--- header.php
+++ header.php
@@ -23,11 +23,14 @@
             ?>
             </figure>
             <div class="entete__navigation">
+
                 <?php wp_nav_menu(array(
                     'menu' => 'principal',
                     'container' => 'nav',
                     'container_class' => 'entete__menu'
                 )); ?>
+                
+                <svg class="menuBurger" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 <?php get_search_form() ?>
 
             </div> <!-- fin entete__navigation  -->
