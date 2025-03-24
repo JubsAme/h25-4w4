a64c91ddbadf29d3e2b11f3550444b5bf42bb9af
JubsAme <e1965295@cmaisonneuve.qc.ca>
2025-03-24 13:59:29 -0400

    Création de la page erreur 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<section class="error404">
+  <h1>Erreur 404 Cette page n'a pas été trouvée!</h1>
+  <h2>Entrez un lien valide ou revenez en arrière!</h2>
+</section>
+    <?php get_footer(); ?>
+   
+</body>
+</html>
