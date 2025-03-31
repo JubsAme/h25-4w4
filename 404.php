e44602a954c8a69326532e1c40e50c331b3fc24b
JubsAme <e1965295@cmaisonneuve.qc.ca>
2025-03-31 13:30:26 -0400

    Examen intra création de la section, du titre et du texte

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+  <section class="section_404">
+    <h1 class="section_404__titre">
+    Oops, vous avez échoué sur l'île 404 !
+    </h1>
+    <p class="section_404__texte">
+    Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !
+    </p>
+  </section>
+<?php get_footer(); ?>
+   
+</body>
+</html>
