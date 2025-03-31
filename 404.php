30944ba79b86621c6340b29033147901b3c745be
JubsAme <e1965295@cmaisonneuve.qc.ca>
2025-03-31 14:12:52 -0400

    Examen Intra ajout du bouton pour le retours vers la page c'accueil et ajout du menu avec les destination

--- 404.php
+++ 404.php
@@ -7,6 +7,13 @@
     <p class="section_404__texte">
     Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !
     </p>
+    <a href="<?php echo home_url(); ?>" class="btn-home">Retour à l'accueil</a>
+    <div class="section_404__menu"></div>
+    <?php wp_nav_menu(array(
+                    'menu' => '404ExamIntra',
+                    'container' => 'a',
+                    'container_class' => 'section_404__menu'
+                )); ?>
   </section>
 <?php get_footer(); ?>
    
