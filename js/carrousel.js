53586f20f8002225fd4f076ea71c27504ac30f26
JubsAme <e1965295@cmaisonneuve.qc.ca>
2025-05-19 21:43:23 -0400

    Ajout d'une animation simple sur le texte lorsque l'image change

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,23 +1,28 @@
 (function(){
   console.log("carrousel.js");
-  let radios = document.querySelectorAll(".hero__radio__input");
-  console.log("hero__radio__input = ", radios.length);
 
+  const radios = document.querySelectorAll(".hero__radio__input");
+  const contenu = document.querySelector(".hero__contenu");
   let currentIndex = 0;
 
   function montrerImage(index) {
     radios[index].checked = true;
+    contenu.classList.remove("animate-text"); // reset animation
+    void contenu.offsetWidth; // force reflow
+    contenu.classList.add("animate-text");
   }
 
-  // change d’image toutes les 5s
+  // Changement automatique toutes les 5 secondes
   setInterval(() => {
     currentIndex = (currentIndex + 1) % radios.length;
     montrerImage(currentIndex);
   }, 5000);
 
+  // Si l’utilisateur sélectionne une image manuellement
   radios.forEach((radio, i) => {
-    radio.addEventListener('change', () => {
-      currentIndex = i; // update l’index actuel pour continuer à partir de là
+    radio.addEventListener("change", () => {
+      currentIndex = i;
+      montrerImage(currentIndex);
     });
   });
 })();
